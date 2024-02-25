0e8b430f5d604e7b758fc916f323976ffe812417
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-25 20:04:50 +0330

    create basic footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { yekan } from "@/utils/fonts";
 import "./globals.css";
 import Header from "@/layout/Header";
+import Footer from "@/layout/Footer";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,8 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={yekan.className}>
-        <Header />
-        {children}
+        <div className="flex flex-col h-screen justify-between">
+          <Header />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,24 +1,7 @@
-import styles from "@/layout/Footer.module.css";
-
 function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.desc}>
-        <h3>سامانه خرید و اجاره ملک</h3>
-        <p>
-          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
-          ستون و سطرآنچنان که لازم است.
-        </p>
-      </div>
-      <div>
-        <ul>
-          <li>تعرفه قانونی</li>
-          <li>دسترسی سریع</li>
-          <li>مشاورین خبره</li>
-          <li>قولنامه محضری</li>
-        </ul>
-      </div>
+    <footer className="flex justify-center items-center w-full">
+      Copy Right All Right Reserved.2024
     </footer>
   );
 }
