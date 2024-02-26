55fa123a122dbf4bd49fce6cce2c70e99ce4ca71
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-26 16:49:20 +0330

    client side authentication in header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,10 +2,10 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { FiLogIn } from "react-icons/fi";
+import { FaUserAlt } from "react-icons/fa";
 
 function Header() {
   const { data } = useSession();
-  console.log(data);
   return (
     <header className="flex justify-between w-full p-2 bg-gray-100">
       <ul className="flex pr-5">
@@ -17,12 +17,21 @@ function Header() {
         </li>
       </ul>
       <ul className="flex pl-5">
-        <li className="p-2">
-          <Link href="/signin" className="flex items-center">
-            <FiLogIn />
-            <span className="p-1">ورود</span>
-          </Link>
-        </li>
+        {data ? (
+          <li className="p-2">
+            <Link href="/dashboard" className="flex items-center">
+              <FaUserAlt />
+              <span className="p-1">پنل کاربری</span>
+            </Link>
+          </li>
+        ) : (
+          <li className="p-2">
+            <Link href="/signin" className="flex items-center">
+              <FiLogIn />
+              <span className="p-1">ورود</span>
+            </Link>
+          </li>
+        )}
       </ul>
     </header>
   );
