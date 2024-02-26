2a578fffb499535f02d285bfc293c254a0b2ee9f
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-26 16:40:38 +0330

    wrapping the next auth provider around the app

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 import Header from "@/layout/Header";
 import Footer from "@/layout/Footer";
 import { Toaster } from "react-hot-toast";
+import NextAuthProvider from "@/providers/NextAuthProvider";
+import Layout from "@/layout/Layout";
 
 export const metadata = {
   title: "Create Next App",
@@ -14,9 +16,10 @@ export default function RootLayout({ children }) {
     <html lang="fa" dir="rtl">
       <body className={yekan.className}>
         <div className="flex flex-col h-screen justify-between">
-          <Header />
-          {children}
-          <Footer />
+          <NextAuthProvider>
+            <Layout>{children}</Layout>
+          </NextAuthProvider>
+
           <Toaster />
         </div>
       </body>
