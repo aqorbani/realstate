a45954dbab2b31911bc703db5162f53148797350
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-26 17:25:33 +0330

    redirect logged in user to main page

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -1,9 +1,11 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import SigninPage from "@/template/SigninPage";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function page() {
   const session = await getServerSession(authOptions);
+  if (session) redirect("/");
   return (
     <div className="flex justify-center items-center w-full h-auto">
       <SigninPage />
--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,6 +1,11 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import SignupPage from "@/template/SignupPage";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
   return (
     <div className="flex justify-center items-center w-full h-auto">
       <SignupPage />
