daed5a0d84fc39976d0a792f47c119591eaa51cb
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-26 17:16:44 +0330

    use getServerSession method in signin page

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -1,6 +1,9 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import SigninPage from "@/template/SigninPage";
+import { getServerSession } from "next-auth";
 
-export default function page() {
+export default async function page() {
+  const session = await getServerSession(authOptions);
   return (
     <div className="flex justify-center items-center w-full h-auto">
       <SigninPage />
