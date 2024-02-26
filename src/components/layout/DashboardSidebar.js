2064684002d8803c10ada822bd0fcf66f30def7c
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-27 00:55:38 +0330

    make dashboard from client side to server side

--- src/components/layout/DashboardSidebar.js
+++ src/components/layout/DashboardSidebar.js
@@ -1,17 +1,17 @@
-"use client";
-import { useSession } from "next-auth/react";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { LuUserSquare2 } from "react-icons/lu";
 
-export default function DashboardSidebar() {
-  const { data } = useSession();
+export default async function DashboardSidebar() {
+  const session = await getServerSession(authOptions);
   return (
-    <div className="shadow-md p-5">
+    <div className="shadow-md p-5 rounded">
       <div className="flex flex-col w-full justify-center items-center border-b-2 border-red-900">
         <div className="text-5xl text-gray-200">
           <LuUserSquare2 />
         </div>
-        <p className="text-gray-500 font-medium">{data?.user?.email}</p>
+        <p className="text-gray-500 font-medium">{session?.user?.email}</p>
       </div>
       <div className="p-2 w-full">
         <Link href="/dashboard">
