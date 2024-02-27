dc25f8b47c236472275f1d2857500e1b3c6c3fa4
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-27 15:23:49 +0330

    create main state to save data

--- src/components/template/AddAdvPage.js
+++ src/components/template/AddAdvPage.js
@@ -1,3 +1,19 @@
+"use client";
+
+import { useState } from "react";
+
 export default function AddAdvPage() {
+  const [advData, setAdvData] = useState({
+    title: "",
+    description: "",
+    location: "",
+    phone: "",
+    price: "",
+    realState: "",
+    constructionDate: new Date(),
+    category: "",
+    rules: [],
+    amenities: [],
+  });
   return <div>AddAdvPage</div>;
 }
