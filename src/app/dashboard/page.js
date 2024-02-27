f15cd3c43531edeef0be93e945e3e87a1ec43bee
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-27 14:50:41 +0330

    show the createdAt date to dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,10 @@
 import DashboardPage from "@/template/DashboardPage";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import User from "@/models/User";
 
-export default function page() {
-  return <DashboardPage />;
+export default async function page() {
+  const session = await getServerSession(authOptions);
+  const user = await User.findOne({ email: session.user.email });
+  return <DashboardPage createdAt={user.createdAt} />;
 }
--- src/components/template/DashboardPage.js
+++ src/components/template/DashboardPage.js
@@ -1,14 +1,10 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
-
-export default async function DashboardPage() {
-  const session = await getServerSession(authOptions);
+export default function DashboardPage({ createdAt }) {
   return (
     <div className="p-5">
       <header className="flex justify-around">
         <div className="flex flex-col justify-center items-center p-2 m-2 shadow-md rounded w-1/5 min-h-28">
           <p className="font-semibold text-lg">تاریخ عضویت</p>
-          <p>محتوا</p>
+          <p>{new Date(createdAt).toLocaleDateString("fa-IR")}</p>
         </div>
         <div className="flex flex-col justify-center items-center p-2 m-2 shadow-md rounded w-1/5 min-h-28">
           <p className="font-semibold text-lg">آگهی</p>
