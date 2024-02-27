ceec1847f4539a76c7b1ee56ddc071f5985406dd
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-27 16:20:14 +0330

    add text input in form and complete the styles

--- src/components/module/TextInput.js
+++ src/components/module/TextInput.js
@@ -3,6 +3,7 @@ import { p2e } from "@/utils/replaceNumber";
 export default function TextInput({
   title,
   name,
+  type = "text",
   advData,
   setAdvData,
   textArea = false,
@@ -13,20 +14,20 @@ export default function TextInput({
   };
 
   return (
-    <div>
+    <div className="mt-7 w-full">
       <p className="font-semibold text-base p-2">{title}</p>
       {textArea ? (
         <textarea
-          className="shadow-sm bg-gray-50 rounded p-2 m-2 min-w-48 w-2/3"
-          type="text"
+          className="shadow-sm bg-gray-50 rounded p-2 m-2 w-full md:w-4/5 lg:w-1/2"
+          type={type}
           name={name}
           value={advData[name]}
           onChange={changeHandler}
         />
       ) : (
         <input
-          className="shadow-sm bg-gray-50 rounded p-2 m-2 min-w-48 w-2/3"
-          type="text"
+          className="shadow-sm bg-gray-50 rounded p-2 m-2 w-full md:w-4/5 lg:w-1/2"
+          type={type}
           name={name}
           value={advData[name]}
           onChange={changeHandler}
--- src/components/template/AddAdvPage.js
+++ src/components/template/AddAdvPage.js
@@ -17,15 +17,63 @@ export default function AddAdvPage() {
     amenities: [],
   });
   return (
-    <div>
+    <div className="w-full">
       <h2 className="font-extrabold text-2xl p-4">ثبت آگهی</h2>
       <TextInput
         title="عنوان آگهی"
         name="title"
+        type="text"
         advData={advData}
         setAdvData={setAdvData}
         textArea={false}
       />
+      <TextInput
+        title="توضیحات آگهی"
+        name="description"
+        type="text"
+        advData={advData}
+        setAdvData={setAdvData}
+        textArea={true}
+      />
+      <TextInput
+        title="آدرس"
+        name="location"
+        type="text"
+        advData={advData}
+        setAdvData={setAdvData}
+        textArea={false}
+      />
+      <TextInput
+        title="شماره تماس"
+        name="phone"
+        type="number"
+        advData={advData}
+        setAdvData={setAdvData}
+        textArea={false}
+      />
+      <TextInput
+        title="قیمت تومان"
+        name="price"
+        type="number"
+        advData={advData}
+        setAdvData={setAdvData}
+        textArea={false}
+      />
+      <TextInput
+        title="بنگاه املاک"
+        name="realState"
+        type="text"
+        advData={advData}
+        setAdvData={setAdvData}
+        textArea={false}
+      />
+      <button
+        type="submit"
+        onClick={() => console.log(advData)}
+        className="w-full md:w-4/5 lg:w-1/2 p-2 m-2 mt-20 mb-20 bg-red-950 text-white rounded hover:bg-red-900 cursor-pointer"
+      >
+        ثبت آگهی
+      </button>
     </div>
   );
 }
