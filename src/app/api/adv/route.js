2cf0d5e1c5cbe9986a3ac3a4ba7d9a69103815e5
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-27 21:02:37 +0330

    create adv api

---
+++ src/app/api/adv/route.js
@@ -0,0 +1,199 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import User from "@/models/User";
+import connectDB from "@/utils/ConnectDB";
+import Advertisement from "@/models/Advertisement";
+import { Schema } from "mongoose";
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const advertisement = await Advertisement.find({ published: true }).select(
+      "-userId"
+    );
+
+    return NextResponse.json(
+      {
+        data: advertisement,
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    const {
+      title,
+      description,
+      location,
+      phone,
+      realState,
+      price,
+      constructionDate,
+      category,
+      amenities,
+      rules,
+    } = await req.json();
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "لطفا وارد حساب کاربری خود شوید",
+        },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+
+    if (
+      !title ||
+      !location ||
+      !description ||
+      !phone ||
+      !realState ||
+      !price ||
+      !constructionDate ||
+      !category
+    ) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر وارد کنید" },
+        { status: 400 }
+      );
+    }
+
+    const newAdvertisement = await Advertisement.create({
+      title,
+      description,
+      location,
+      phone,
+      realState,
+      constructionDate,
+      amenities,
+      rules,
+      category,
+      price: +price,
+      userId: new Schema.Types.ObjectId(user._id),
+    });
+    console.log(newAdvertisement);
+    return NextResponse.json(
+      { message: "آگهی جدید اضافه شد" },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(req) {
+  try {
+    await connectDB();
+
+    const {
+      _id,
+      title,
+      description,
+      location,
+      phone,
+      realState,
+      price,
+      constructionDate,
+      category,
+      amenities,
+      rules,
+    } = await req.json();
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "لطفا وارد حساب کاربری خود شوید",
+        },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+
+    if (
+      !_id ||
+      !title ||
+      !location ||
+      !description ||
+      !phone ||
+      !realState ||
+      !price ||
+      !constructionDate ||
+      !category
+    ) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر وارد کنید" },
+        { status: 400 }
+      );
+    }
+
+    const advertisement = await Advertisement.findOne({ _id });
+    if (!user._id.equals(advertisement.userId)) {
+      return NextResponse.json(
+        {
+          error: "دستری شما به این آگهی محدود شده است",
+        },
+        { status: 403 }
+      );
+    }
+
+    advertisement.title = title;
+    advertisement.description = description;
+    advertisement.location = location;
+    advertisement.phone = phone;
+    advertisement.realState = realState;
+    advertisement.price = price;
+    advertisement.constructionDate = constructionDate;
+    advertisement.amenities = amenities;
+    advertisement.rules = rules;
+    advertisement.category = category;
+    advertisement.save();
+
+    return NextResponse.json(
+      {
+        message: "آگهی با موفقیت ویرایش شد",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
