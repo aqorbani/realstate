fc945d576cc380283a14fcd54b5a89617f363135
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-28 20:14:29 +0330

    create detail page components

--- src/app/properties/[advId]/page.js
+++ src/app/properties/[advId]/page.js
@@ -1,3 +1,15 @@
-export default function page() {
-  return <div>page</div>;
+import Advertisement from "@/models/Advertisement";
+import DetailPage from "@/template/DetailPage";
+import connectDB from "@/utils/ConnectDB";
+
+export default async function page({ params: { advId } }) {
+  await connectDB();
+  const adv = await Advertisement.findOne({ _id: advId });
+  if (!adv)
+    return (
+      <p className="font-semibold text-base text-gray-600 p-5">
+        محتوایی برای نمایش وجود ندارد
+      </p>
+    );
+  return <DetailPage data={adv} />;
 }
---
+++ src/components/template/DetailPage.js
@@ -0,0 +1,3 @@
+export default function DetailPage({ data }) {
+  return <div>DetailPage</div>;
+}
